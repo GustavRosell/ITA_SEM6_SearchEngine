52497b1242c9b78e1166303f4af240ee5c70bff4
rosell <Gustavrosell@gmail.com>
2025-08-27 12:52:14 +0200

    test 3

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 namespace Shared
 {
     public class Paths
     {
-        public static string DATABASE = @"C:\Users\Gusta\OneDrive\Dokumenter\GitHub\SearchEngine-main\Data\searchDB.db";
+        public static string DATABASE
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return @"C:\Users\Gusta\OneDrive\Dokumenter\GitHub\SearchEngine-main\Data\searchDB.db";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return @"/Users/rosell/ITA_SEM6_SearchEngine/Data/searchDB.db";
+                }
+                else
+                {
+                    // Default to Linux/Unix style path
+                    return @"/Users/rosell/ITA_SEM6_SearchEngine/Data/searchDB.db";
+                }
+            }
+        }
     }
 }
